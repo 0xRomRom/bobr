81384869d821ca405b62c0ff83c2220a66e5bd1c
Roman - <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>
2024-10-01 05:15:51 +0200

    Modal created

--- src/components/Mememaker/MemeMaker.jsx
+++ src/components/Mememaker/MemeMaker.jsx
@@ -1,7 +1,23 @@
 import stl from "./MemeMaker.module.css";
+import { Canvas, FabricImage, ActiveSelection, Textbox } from "fabric";
+import { FiUpload } from "react-icons/fi";
 
 const MemeMaker = () => {
-  return <div className={stl.mememaker}></div>;
+  return (
+    <div className={stl.mememaker}>
+      <h1>BOBR Maker</h1>
+      <div className={stl.makerModal}>
+        <div className={stl.canvasWrap}>
+          <span className={stl.uploadSpan}>
+            <FiUpload />
+            Upload Image
+          </span>
+        </div>
+        <div className={stl.bar}></div>
+        <div className={stl.bar}></div>
+      </div>
+    </div>
+  );
 };
 
 export default MemeMaker;
